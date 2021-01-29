2fd8f3c2fe318707854795da95185dcf58e72f8c
isaacTadela <47701407+isaacTadela@users.noreply.github.com>
2021-01-29 17:26:25 +0200

    fix clean_env file

--- clean_environment.py
+++ clean_environment.py
@@ -1,6 +1,15 @@
 import requests
 
-requests.get('http://127.0.0.1:5001/stop_server')
-requests.get('http://127.0.0.1:5000/stop_server')
+# stop rest app
+try:
+    requests.get('http://127.0.0.1:5000/stop_server')
+except Exception:
+    pass
+
+# stop web app
+try:
+    requests.get('http://127.0.0.1:5001/stop_server')
+except Exception:
+    pass
 
 
